cc6490c237ca4155e6344acb193aedaef03396e7
Hashim-Muneeb <hashimmuneeb75@gmail.com>
2025-01-27 18:32:43 +0500

    COMPLETED

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
